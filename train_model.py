c5e34aa28da4d52b090a75ac906a516d32178c9b
Sudharsana R <24cs219@kpriet.ac.in>
2026-02-28 12:09:36 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,23 +3,17 @@ from sklearn.tree import DecisionTreeClassifier
 import joblib
 
 
-
-# Load dataset
 data = pd.read_csv("learning_data_balanced.csv")
 
-# Features (Input)
 X = data[["watches_videos", "prefers_audio", "likes_practical", "reads_notes"]]
 
-# Target (Output)
 y = data["learning_style"]
 
-# Create Decision Tree model
 model = DecisionTreeClassifier()
 
-# Train model
 model.fit(X, y)
 
-# Save model
 joblib.dump(model, "model.pkl")
+
 
 print("Model trained and saved successfully!")
